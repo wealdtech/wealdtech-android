ac6b53540f07a5e240e7649dc94439e70654a977
Jim McDonald <Jim@mcdee.net>
2014-08-11 01:39:40 +0100

    Added more tests

--- wealdtech-fabric/src/androidTest/java/com/wealdtech/android/fabric/test/GlobalScopeTest.java
+++ wealdtech-fabric/src/androidTest/java/com/wealdtech/android/fabric/test/GlobalScopeTest.java
@@ -2,7 +2,11 @@ package com.wealdtech.android.fabric.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
 import com.wealdtech.android.fabric.Fabric;
+
+import java.util.Date;
+import java.util.Map;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -34,6 +38,20 @@ public class GlobalScopeTest extends ActivityInstrumentationTestCase2<GlobalScop
     final ImmutableSet<Integer> testSet = ImmutableSet.of(1, 2, 3);
     Fabric.getInstance().set("test set", testSet);
     assertThat(Fabric.getInstance().get("test set").equals(testSet));
+
+    activity.finish();
+  }
+
+  public void testComplexType()
+  {
+    final GlobalScopeTestActivity activity = getActivity();
+
+    final Map<String, Date> testMap = Maps.newHashMap();
+    testMap.put("test 1", new Date(1000000000000L));
+    testMap.put("test 2", new Date(2000000000000L));
+
+    Fabric.getInstance().set("test map", testMap);
+    assertThat(Fabric.getInstance().<Map<String, Date>>get("test map").equals(testMap));
 
     activity.finish();
   }
