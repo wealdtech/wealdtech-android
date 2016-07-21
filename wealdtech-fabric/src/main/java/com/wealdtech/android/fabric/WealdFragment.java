9a689d99355106af1335bf0b392fff34030d00be
Jim McDonald <Jim@mcdee.net>
2016-07-21 20:56:51 +0100

    Reworked weald fragment to provide additional error and empty views

--- wealdtech-fabric/src/main/java/com/wealdtech/android/fabric/WealdFragment.java
+++ wealdtech-fabric/src/main/java/com/wealdtech/android/fabric/WealdFragment.java
@@ -14,68 +14,60 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static com.wealdtech.Preconditions.checkState;
 
 /**
  * A fragment with additional features:
  * <ul>
- *   <li>Progress dialog when loading data</li>
+ *   <li>Separate content views for progress, display, error and no data (empty)</li>
  * </ul>
  */
 public abstract class WealdFragment extends Fragment
 {
   private static final Logger LOG = LoggerFactory.getLogger(WealdFragment.class);
 
+  public static final int PROGRESS = 1;
+  public static final int CONTENT = 2;
+  public static final int EMPTY = 3;
+  public static final int ERROR = 4;
+
   private LinearLayout progressContainer;
   private String pendingText = null;
   private TextView progressText;
   private ViewGroup contentContainer;
+  private ViewGroup emptyContainer;
+  private ViewGroup errorContainer;
   private View contentView;
-  private boolean contentShown = true;
+  private View errorView;
+  private View emptyView;
+  // The current shown content
+  private int shown;
 
   @Override
   public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
   {
     final ViewGroup view = (ViewGroup)inflater.inflate(R.layout.weald_fragment, container, false);
     progressContainer = findById(view, R.id.progress_container);
+    contentContainer = findById(view, R.id.content_container);
+    emptyContainer = findById(view, R.id.empty_container);
+    errorContainer = findById(view, R.id.error_container);
+
     progressText = findById(progressContainer, R.id.progress_text);
     if (pendingText != null)
     {
       progressText.setText(pendingText);
       pendingText = null;
     }
-    contentContainer = findById(view, R.id.content_container);
 
     // Display the correct container
-    progressContainer.setVisibility(contentShown ? View.GONE : View.VISIBLE);
-    contentContainer.setVisibility(contentShown ? View.VISIBLE : View.GONE);
+    progressContainer.setVisibility(shown == PROGRESS ? GONE : VISIBLE);
+    contentContainer.setVisibility(shown == CONTENT ? VISIBLE : GONE);
+    emptyContainer.setVisibility(shown == EMPTY ? VISIBLE : GONE);
+    errorContainer.setVisibility(shown == ERROR ? VISIBLE : GONE);
     return view;
   }
-
-  @Override
-  public void onViewCreated(final View view, final Bundle savedInstanceState)
-  {
-    super.onViewCreated(view, savedInstanceState);
-  }
-
-//  @Override
-//  public void onAttach(final Activity activity)
-//  {
-//    super.onAttach(activity);
-//    if ((activity instanceof CognisFragmentListener))
-//    {
-//      fragmentListener = (CognisFragmentListener)getActivity();
-//    }
-//    fragmentListener.onFragmentAttached(this);
-//  }
-//
-//  @Override
-//  public void onDetach()
-//  {
-//    super.onDetach();
-//    fragmentListener.onFragmentDetached(this);
-//    fragmentListener = DUMMY_LISTENER;
-//  }
 
   @Override
   public void onDestroyView()
@@ -83,7 +75,8 @@ public abstract class WealdFragment extends Fragment
     progressContainer = null;
     progressText = null;
     contentContainer = null;
-    contentShown = false;
+    emptyContainer = null;
+    errorContainer = null;
     super.onDestroyView();
   }
 
@@ -186,57 +179,193 @@ public abstract class WealdFragment extends Fragment
     contentView = view;
   }
 
-  public boolean isContentShown(){ return contentShown; }
-
-  public void setContentShown(final boolean shown)
+  @SuppressWarnings("unchecked")
+  public <T extends View> T setErrorView(final int layoutResId)
   {
-    setContentShown(shown, true);
+    LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+    T errorView = (T)layoutInflater.inflate(layoutResId, null);
+    setErrorView(errorView);
+    return errorView;
   }
 
-  private void setContentShown(final boolean shown, final boolean animate)
+  public void setErrorView(final View view)
   {
-    if (contentShown == shown)
+    checkState(view != null, "Cannot set error content to NULL");
+    checkState(view instanceof ViewGroup, "Cannot use non-container for error content");
+
+    if (errorView == null)
     {
+      errorContainer.addView(view);
+    }
+    else
+    {
+      final int index = errorContainer.indexOfChild(errorView);
+      // replace existing view
+      errorContainer.removeView(errorView);
+      errorContainer.addView(view, index);
+    }
+    errorView = view;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T extends View> T setEmptyView(final int layoutResId)
+  {
+    LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+    T emptyView = (T)layoutInflater.inflate(layoutResId, null);
+    setEmptyView(emptyView);
+    return emptyView;
+  }
+
+  public void setEmptyView(final View view)
+  {
+    checkState(view != null, "Cannot set empty content to NULL");
+    checkState(view instanceof ViewGroup, "Cannot use non-container for empty content");
+
+    if (emptyView == null)
+    {
+      emptyContainer.addView(view);
+    }
+    else
+    {
+      final int index = emptyContainer.indexOfChild(emptyView);
+      // replace existing view
+      emptyContainer.removeView(emptyView);
+      emptyContainer.addView(view, index);
+    }
+    emptyView = view;
+  }
+
+  public boolean isContentShown(){ return shown == CONTENT; }
+
+  public boolean isProgressShown(){ return shown == PROGRESS; }
+
+  public boolean isEmptyShown(){ return shown == EMPTY; }
+
+  public boolean isErrorShown(){ return shown == ERROR; }
+
+  public void showContent() { show(CONTENT, true); }
+
+  public void showProgress() { show(PROGRESS, true); }
+
+  public void showEmpty() { show(EMPTY, true); }
+
+  public void showError() { show(ERROR, true); }
+
+  private void show(final int toShow, final boolean animate)
+  {
+    if (toShow == shown)
+    {
+      // Already showing this
       return;
     }
-    contentShown = shown;
 
-    // Because setContentShown() is commonly called in loaders, which are commonly set up in onCreate(), there
-    // is no guarantee that our view is set up yet.  Our onCreateView() handles this situation by setting the
-    // correct views depending on the value of contentShown
-    if (progressContainer != null && contentContainer != null)
+    if ((toShow == PROGRESS && progressContainer == null) || (toShow == CONTENT && contentContainer == null) ||
+        (toShow == ERROR && errorContainer == null) || (toShow == EMPTY && emptyContainer == null))
     {
-      if (shown)
-      {
+      // It is possible that we have been called prior to the views being inflated.  This is fine; we leave a note as
+      // to which view is to be shown and it will be handled in onCreateView()
+      shown = toShow;
+      return;
+    }
+
+    // First off get rid of whatever we need to get rid of
+    switch(shown)
+    {
+      case PROGRESS:
         if (animate)
         {
           progressContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_out));
-          contentContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
         }
         else
         {
           progressContainer.clearAnimation();
-          contentContainer.clearAnimation();
         }
-        progressContainer.setVisibility(View.GONE);
-        contentContainer.setVisibility(View.VISIBLE);
-      }
-      else
-      {
+        progressContainer.setVisibility(GONE);
+        break;
+      case CONTENT:
         if (animate)
         {
-          progressContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
           contentContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_out));
         }
         else
         {
-          progressContainer.clearAnimation();
           contentContainer.clearAnimation();
         }
-        progressContainer.setVisibility(View.VISIBLE);
-        contentContainer.setVisibility(View.GONE);
-      }
+        contentContainer.setVisibility(GONE);
+        break;
+      case EMPTY:
+        if (animate)
+        {
+          emptyContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_out));
+        }
+        else
+        {
+          emptyContainer.clearAnimation();
+        }
+        emptyContainer.setVisibility(GONE);
+        break;
+      case ERROR:
+        if (animate)
+        {
+          errorContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_out));
+        }
+        else
+        {
+          errorContainer.clearAnimation();
+        }
+        errorContainer.setVisibility(GONE);
+        break;
     }
+
+    // Now show whatever we need to show
+    switch(toShow)
+    {
+      case PROGRESS:
+        if (animate)
+        {
+          progressContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
+        }
+        else
+        {
+          progressContainer.clearAnimation();
+        }
+        progressContainer.setVisibility(VISIBLE);
+        break;
+      case CONTENT:
+        if (animate)
+        {
+          contentContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
+        }
+        else
+        {
+          contentContainer.clearAnimation();
+        }
+        contentContainer.setVisibility(VISIBLE);
+        break;
+      case EMPTY:
+        if (animate)
+        {
+          emptyContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
+        }
+        else
+        {
+          emptyContainer.clearAnimation();
+        }
+        emptyContainer.setVisibility(VISIBLE);
+        break;
+      case ERROR:
+        if (animate)
+        {
+          errorContainer.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
+        }
+        else
+        {
+          errorContainer.clearAnimation();
+        }
+        errorContainer.setVisibility(VISIBLE);
+        break;
+    }
+    shown = toShow;
   }
 
   public void setWaitingText(final String text)
